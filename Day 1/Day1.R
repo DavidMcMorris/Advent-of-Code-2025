26a52cc1576c37d588e6c531f0a0d580c854d68c
DavidMcMorris <david.mcmorris92@gmail.com>
2025-12-01 12:37:19 -0500

    Finished part 2

--- Day 1/Day1.R
+++ Day 1/Day1.R
@@ -1,16 +1,22 @@
 library(dplyr)
 
-input_filename <- "input.txt"
-input <- read.table(input_filename)[[1]]
+input_file <- "input.txt"
+input <- read.table(input_file)[[1]]
 input <- gsub("L", "-", input) %>% gsub("R", "+", .) %>% as.numeric()
 
-counter <- 0
+extra <- 0
+counter <- c(0, 0)
 value <- 50
 for (i in seq_along(input)) {
-  value <- (value + input[i]) %% 100
-  if (value == 0) {
-    counter <- counter + 1
+  turns <- abs(input[i]) %/% 100
+  spin <- sign(input[i]) * (abs(input[i]) %% 100)
+  new_value <- (value + spin) %% 100
+  if (new_value == 0) {
+    counter <- counter + c(1, 1)
+  } else if ((sign(spin) * (new_value - value)) < 0 && value != 0) {
+    counter <- counter + c(0, 1)
   }
+  value <- new_value
+  extra <- extra + turns
 }
-
-print(counter)
+print(counter + c(0, extra))
