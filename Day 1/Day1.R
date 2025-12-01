80e91a2062cb827f609c7f9a552a8c7f639595de
DavidMcMorris <david.mcmorris92@gmail.com>
2025-12-01 12:43:55 -0500

    streamlined

--- Day 1/Day1.R
+++ Day 1/Day1.R
@@ -4,19 +4,16 @@ input_file <- "input.txt"
 input <- read.table(input_file)[[1]]
 input <- gsub("L", "-", input) %>% gsub("R", "+", .) %>% as.numeric()
 
-extra <- 0
 counter <- c(0, 0)
 value <- 50
 for (i in seq_along(input)) {
-  turns <- abs(input[i]) %/% 100
-  spin <- sign(input[i]) * (abs(input[i]) %% 100)
-  new_value <- (value + spin) %% 100
+  counter <- counter + c(0, abs(input[i]) %/% 100)
+  new_value <- (value + input[i]) %% 100
   if (new_value == 0) {
     counter <- counter + c(1, 1)
-  } else if ((sign(spin) * (new_value - value)) < 0 && value != 0) {
+  } else if ((sign(input[i]) * (new_value - value)) < 0 && value != 0) {
     counter <- counter + c(0, 1)
   }
   value <- new_value
-  extra <- extra + turns
 }
-print(counter + c(0, extra))
+print(counter)
