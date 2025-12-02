93699ef49390d0ee1b12b660bcf681c791202901
DavidMcMorris <david.mcmorris92@gmail.com>
2025-12-02 12:37:28 -0500

    finder function works for numbers in a range with endpoints of the same magnitude

--- Day 2/Day2.R
+++ Day 2/Day2.R
@@ -7,3 +7,14 @@ input <- read.csv(input_file, header = FALSE) %>%
   unlist() %>%
   matrix(ncol = 2, byrow = TRUE) %>%
   apply(., 2, as.numeric)
+
+finder <- function(a, b) {
+  a_len <- nchar(a)
+  b_len <- nchar(b)
+  if (a_len == b_len) {
+    fac <- 10^(a_len / 2) + 1
+    low <- ceiling(a / fac)
+    high <- floor(b / fac)
+    fac * (low:high)
+  }
+}
