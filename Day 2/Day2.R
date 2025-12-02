c47c440a5d9eec1ebc66c26b86a520ca2bc70adb
DavidMcMorris <david.mcmorris92@gmail.com>
2025-12-02 15:23:07 -0500

    returns the correct (invalid) ID's plus some extras.

--- Day 2/Day2.R
+++ Day 2/Day2.R
@@ -8,13 +8,26 @@ input <- read.csv(input_file, header = FALSE) %>%
   matrix(ncol = 2, byrow = TRUE) %>%
   apply(., 2, as.numeric)
 
-finder <- function(a, b) {
-  a_len <- nchar(a)
-  b_len <- nchar(b)
+digs <- function(x) {
+  floor(log10(x)) + 1
+}
+
+finder <- function(x) {
+  a <- x[1]
+  b <- x[2]
+  a_len <- digs(a)
+  b_len <- digs(b)
   if (a_len == b_len) {
-    fac <- 10^(a_len / 2) + 1
-    low <- ceiling(a / fac)
-    high <- floor(b / fac)
-    fac * (low:high)
+    if (a_len %% 2 == 0) {
+      fac <- 10^(a_len / 2) + 1
+      low <- ceiling(a / fac)
+      high <- floor(b / fac)
+      fac * (low:high)
+    } else {
+      NA
+    }
+  } else {
+    new_b <- 10^(b_len - 1) - 1
+    c(finder(c(a, new_b)), finder(c(new_b + 1, b)))
   }
 }
