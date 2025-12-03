f56948edee62026d56e27bb4d734ee712c6caee7
DavidMcMorris <david.mcmorris92@gmail.com>
2025-12-03 12:58:03 -0500

    Have a working next_bat function to find next digit.

--- Day 3/Day 3.R
+++ Day 3/Day 3.R
@@ -4,13 +4,23 @@ digs <- function(x) {
   floor(log10(x)) + 1
 }
 
-input_file <- "input.txt"
+input_file <- "sample.txt"
 len <- scan(input_file, nmax = 1) %>% digs()
 input <- readLines(input_file) %>%
   strsplit(split = "") %>%
   unlist() %>%
   as.numeric() %>%
   matrix(ncol = len, byrow = TRUE)
+
+next_bat <- function(x, l) {
+  a <- max(x[1:(length(x) - l + 1)])
+  if (l == 1) {
+    a
+  } else {
+    a_ind <- which(x == a)[1]
+    c(a, next_bat(x[-(1:a_ind)], l - 1))
+  }
+}
 
 total <- 0
 for (i in seq_len(nrow(input))) {
@@ -20,10 +30,3 @@ for (i in seq_len(nrow(input))) {
   total <- total + a * 10 + b
 }
 print(total)
-
-jolt <- function(x)
-  y <- NULL
-  for (i in 1:12) {
-    a <- max(x[1:(len - 13 + i)])
-    a_ind <- which(input[i, ] == a)[1]
-  }
