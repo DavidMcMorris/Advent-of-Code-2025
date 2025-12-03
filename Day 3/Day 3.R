157b2f2d02b1cd946f98f4b54575e8ad1e38d4e4
DavidMcMorris <david.mcmorris92@gmail.com>
2025-12-03 11:38:27 -0500

    Part 1 works

---
+++ Day 3/Day 3.R
@@ -0,0 +1,22 @@
+library(dplyr)
+
+digs <- function(x) {
+  floor(log10(x)) + 1
+}
+
+input_file <- "input.txt"
+len <- scan(input_file, nmax = 1) %>% digs()
+input <- readLines(input_file) %>%
+  strsplit(split = "") %>%
+  unlist() %>%
+  as.numeric() %>%
+  matrix(ncol = len, byrow = TRUE)
+
+total <- 0
+for (i in seq_len(nrow(input))) {
+  a <- max(input[i, -len])
+  a_ind <- which(input[i, ] == a)[1]
+  b <- max(input[i, -(1:a_ind)])
+  total <- total + a * 10 + b
+}
+print(total)
