a51919941295e9b8b3f870fb5c9f592b5945c16f
DavidMcMorris <david.mcmorris92@gmail.com>
2025-12-04 11:31:54 -0500

    Finished part 2

--- Day 4/Day4.R
+++ Day 4/Day4.R
@@ -15,15 +15,34 @@ adjacent_inds <- function(ind, dim) {
   adj_inds[rowSums(adj_inds > dim | adj_inds < 1) == 0, ]
 }
 
-total <- 0
+total_acc <- 0
 for (i in 1:len^2) {
   if (input[i] == "@") {
     current_ind <- arrayInd(i, c(len, len))
     adj_inds <- adjacent_inds(current_ind, len)
     adj <- (input[adj_inds] == "@") %>% sum()
     if (adj < 4) {
-      total <- total + 1
+      total_acc <- total_acc + 1
     }
   }
 }
-print(total)
+print(total_acc)
+
+total_rem <- 0
+flag <- 1
+while (flag == 1) {
+  flag <- 0
+  for (i in 1:len^2) {
+    if (input[i] == "@") {
+      current_ind <- arrayInd(i, c(len, len))
+      adj_inds <- adjacent_inds(current_ind, len)
+      adj <- (input[adj_inds] == "@") %>% sum()
+      if (adj < 4) {
+        input[i] <- "."
+        total_rem <- total_rem + 1
+        flag <- 1
+      }
+    }
+  }
+}
+print(total_rem)
