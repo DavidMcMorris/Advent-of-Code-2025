46ccf7473954620da113a5df1f1407dee2e3f709
DavidMcMorris <david.mcmorris92@gmail.com>
2025-12-04 10:39:53 -0500

    started writing a function to find adjacent indices

--- Day 4/Day4.R
+++ Day 4/Day4.R
@@ -1,9 +1,16 @@
 library(dplyr)
 
-input_file <- "input.txt"
+input_file <- "sample.txt"
 len <- readLines(input_file, 1) %>% nchar()
 input <- read.table(input_file) %>%
   as.matrix() %>%
   strsplit(split = "") %>%
   unlist() %>%
   matrix(ncol = len, byrow = TRUE)
+
+adjacent_inds <- function(ind, dim) {
+  if(ind > dim && ind %% dim > 1 && ind < dim^2 - dim) {
+    ad_inds <- c(ind, ind + 1, ind - 1) %>% c(., . + dim, . - dim)
+    ad_inds <- setdiff(ad_inds, ind)
+  } else if(ind < )
+}
