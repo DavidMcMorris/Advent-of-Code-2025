a2d1389d054d70d797c70ee40d7013ecaea8bb3a
DavidMcMorris <david.mcmorris92@gmail.com>
2025-12-05 15:36:27 -0500

    Works for sample

--- Day 5/Day5.R
+++ Day 5/Day5.R
@@ -13,3 +13,18 @@ ids <- input[(cut + 1):length(input)] %>%
   as.numeric()
 
 rm(input)
+
+counter <- 0
+
+while (length(ids) > 0) {
+  for (j in seq_len(nrow(ranges))) {
+    if (ids[1] >= ranges[j, 1] && ids[1] <= ranges[j, 2]) {
+      counter <- counter + 1
+      ids <- ids[-1]
+    } else if (j == nrow(ranges)) {
+      ids <- ids[-1]
+    }
+  }
+}
+
+print(counter)
