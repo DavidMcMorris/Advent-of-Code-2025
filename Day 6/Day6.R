640cbb9ed358140034641a4ee4a19793905ef489
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2025-12-07 11:04:01 -0500

    Part 2 works on sample, but need to account for different number of numbers in each problem for full input.

--- Day 6/Day6.R
+++ Day 6/Day6.R
@@ -1,10 +1,23 @@
 library(dplyr)
 
 input_file <- "input.txt"
+options(digits = 20)
 input <- read.table(input_file)
 ops <- input[nrow(input), ]
 input <- input[-nrow(input), ] %>% apply(., 2, as.numeric)
-
 plus <- which(ops == "+")
 
-apply(input[, -plus], 2, prod) %>% sum(., input[, plus]) %>% print(digits = 20)
+input_2 <- readLines(input_file)
+input_2 <- input_2[-length(input_2)] %>% strsplit(split = "")
+len <- length(input_2[[1]])
+input_2 <- input_2 %>%
+  unlist() %>%
+  matrix(ncol = len, byrow = TRUE) %>%
+  apply(., 2, paste, collapse = "") %>%
+  as.numeric()
+
+n <- which(is.na(input_2))[1] - 1
+input_2 <- input_2[which(!is.na(input_2))] %>% matrix(., nrow = n)
+
+apply(input[, -plus], 2, prod) %>% sum(., input[, plus]) %>% print()
+apply(input_2[, -plus], 2, prod) %>% sum(., input_2[, plus]) %>% print()
