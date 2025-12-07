ad38b778e8083dfa16d98340296b61dd082b10b8
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2025-12-07 10:13:07 -0500

    part 1 complete

--- Day 6/Day6.R
+++ Day 6/Day6.R
@@ -1,4 +1,10 @@
 library(dplyr)
 
-input_file <- "sample.txt"
-input <- read.delim(input_file, header = FALSE, sep = " ")
+input_file <- "input.txt"
+input <- read.table(input_file)
+ops <- input[nrow(input), ]
+input <- input[-nrow(input), ] %>% apply(., 2, as.numeric)
+
+plus <- which(ops == "+")
+
+apply(input[, -plus], 2, prod) %>% sum(., input[, plus]) %>% print(digits = 20)
