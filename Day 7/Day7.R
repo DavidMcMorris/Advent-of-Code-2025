53ee1409a1af9f52ce4ed2f791e7493755c4e55d
DavidMcMorris <david.mcmorris92@gmail.com>
2025-12-09 12:15:48 -0500

    Added path counting, works for sample

--- Day 7/Day7.R
+++ Day 7/Day7.R
@@ -32,16 +32,16 @@ splitters <- which(input == "^")
 sum(input[splitters - 1] == "|") %>% print()
 
 nodes <- c(splitters, seq(nrow(input), length(input), by = nrow(input)))
-splitter_inds <- arrayInd(nodes, dims)
-splitter_inds <- splitter_inds[order(splitter_inds[, 1]), ]
-num_nodes <- nrow(splitter_inds)
+node_inds <- arrayInd(nodes, dims)
+node_inds <- node_inds[order(node_inds[, 1]), ]
+num_nodes <- nrow(node_inds)
 
 adj_mat <- matrix(0, nrow = num_nodes, ncol = num_nodes)
 
-for (i in seq_along(splitter_inds[, 1])) {
-  below_cond <- splitter_inds[, 1] > splitter_inds[i, 1]
-  left_cond <- splitter_inds[, 2] == splitter_inds[i, 2] - 1
-  right_cond <- splitter_inds[, 2] == splitter_inds[i, 2] + 1
+for (i in seq_along(node_inds[, 1])) {
+  below_cond <- node_inds[, 1] > node_inds[i, 1]
+  left_cond <- node_inds[, 2] == node_inds[i, 2] - 1
+  right_cond <- node_inds[, 2] == node_inds[i, 2] + 1
   below_l <- which(below_cond & left_cond)
   below_r <- which(below_cond & right_cond)
   if (length(below_l) > 0) {
@@ -52,17 +52,25 @@ for (i in seq_along(splitter_inds[, 1])) {
   }
 }
 
-visited <- rep(FALSE, nrow(splitter_inds))
+visited <- rep(FALSE, nrow(node_inds))
 
+paths <- 0
 dfs <- function(node) {
   if (visited[node]) {
     break
   } else {
+    if(node > (length(nodes) - nrow(input) + 1)) {
+      paths <<- paths + 1
+    }
     visited[node] <- TRUE
-    print(node)
+    visited_current <- visited
     neighbors <- which(adj_mat[node, ] == 1)
     for (i in neighbors) {
       dfs(i)
+      visited <- visited_current
     }
   }
+  paths
 }
+
+dfs(1) %>% print
