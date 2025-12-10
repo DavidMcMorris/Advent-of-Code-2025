d2864e1a314754c1740f1b12305257adae5690ff
DavidMcMorris <david.mcmorris92@gmail.com>
2025-12-10 13:50:14 -0500

    Finished part 1

--- Day 9/Day9.R
+++ Day 9/Day9.R
@@ -1,2 +1,15 @@
 input_file <- "input.txt"
-input <- read.csv(input_file, header = FALSE)
+input <- read.csv(input_file, header = FALSE) |> as.matrix()
+
+area <- function(a, b) {
+  dx <- abs(a[1] - b[1]) + 1
+  dy <- abs(a[2] - b[2]) + 1
+  dx * dy
+}
+
+max_area <- 0
+for (i in seq_len(nrow(input))) {
+  for (j in seq_len(nrow(input))) {
+    max_area <- max(max_area, area(input[i, ], input[j, ]))
+  }
+}
