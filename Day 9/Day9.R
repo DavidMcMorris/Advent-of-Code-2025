5358bafb06b310235f5b30f4d7fd8c063dbf0e25
DavidMcMorris <david.mcmorris92@gmail.com>
2025-12-10 16:09:19 -0500

    Found boundary

--- Day 9/Day9.R
+++ Day 9/Day9.R
@@ -1,4 +1,4 @@
-input_file <- "input.txt"
+input_file <- "sample.txt"
 input <- read.csv(input_file, header = FALSE) |> as.matrix()
 
 area <- function(a, b) {
@@ -13,3 +13,16 @@ for (i in seq_len(nrow(input))) {
     max_area <- max(max_area, area(input[i, ], input[j, ]))
   }
 }
+
+input <- rbind(input, input[1, ])
+boundary <- NULL
+for (i in 1:(nrow(input) - 1)) {
+  d <- which(input[i, ] != input[i + 1, ])
+  s <- seq(from = input[i, d], to = input[i + 1, d])
+  if (d == 1) {
+    boundary <- rbind(boundary, cbind(s[1:(length(s) - 1)], input[i, 2]))
+  } else {
+    boundary <- rbind(boundary, cbind(input[i, 1], s[1:(length(s) - 1)]))
+  }
+}
+boundary <- rbind(boundary, input[1, ])
