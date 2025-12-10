b40928672805d702a2a00f8377af918b1c3c713c
DavidMcMorris <david.mcmorris92@gmail.com>
2025-12-10 12:02:05 -0500

    Slow, and relies on the igraph package, but works

--- Day 8/Day8.R
+++ Day 8/Day8.R
@@ -1,6 +1,29 @@
-input_file <- "sample.txt"
+library(igraph)
+input_file <- "input.txt"
 input <- read.csv(input_file, header = FALSE)
 
 distances <- dist(input, method = "euclidean") |> as.matrix()
 dims <- dim(distances)
-links <- matrix(0, nrow = dims[1], ncol = dims[2])
+links <- matrix(1, nrow = dims[1], ncol = dims[2])
+
+i <- 0
+flag <- 0
+while (flag == 0) {
+  unlinked <- distances * links
+  ind <- which(unlinked == min(unlinked[unlinked != 0]))
+  if (length(ind) > 1) {
+    links[ind] <- 0
+  }
+  c <- abs(links - 1) |> graph_from_adjacency_matrix() |> count_components()
+  if (c == 1) {
+    join <- input[arrayInd(ind[1], dims)[1, ], ]
+    prod(join[, 1]) |> print()
+    flag <- 1
+  }
+  i <- i + 1
+  if (i == 1000) {
+    x <- abs(links - 1) |> graph_from_adjacency_matrix() |> components()
+    x <- x$csize |> sort() |> rev()
+    prod(x[1:3]) |> print()
+  }
+}
