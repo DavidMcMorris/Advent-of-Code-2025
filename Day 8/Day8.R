85b522dd6c82ad0277178c9a9dbfbaa0e84ddc1e
DavidMcMorris <david.mcmorris92@gmail.com>
2025-12-10 08:57:50 -0500

    Computed distances

--- Day 8/Day8.R
+++ Day 8/Day8.R
@@ -1,2 +1,6 @@
 input_file <- "sample.txt"
 input <- read.csv(input_file, header = FALSE)
+
+distances <- dist(input, method = "euclidean") |> as.matrix()
+dims <- dim(distances)
+links <- matrix(0, nrow = dims[1], ncol = dims[2])
