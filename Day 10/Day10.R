1f7d1f03e0191fca55af5f4f55594f287eba9962
DavidMcMorris <david.mcmorris92@gmail.com>
2025-12-20 15:29:59 -0500

    Parsed input to a list

--- Day 10/Day10.R
+++ Day 10/Day10.R
@@ -1,1 +1,11 @@
 input_file <- "sample.txt"
+input <- readLines(input_file) |> strsplit(split = " ")
+
+machines <- list()
+
+for (i in seq_along(input)) {
+  l <- which((input[[i]][1] |> strsplit(split = ""))[[1]] == "#")
+  b <- input[[i]][-c(1, length(input[[i]]))]
+  j <- input[[i]][length(input[[i]])]
+  machines[[i]] <- list("lights" = l - 2, "buttons" = b, "joltage" = j)
+}
