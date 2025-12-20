73713ff1605c364629917074bf767db44142ad48
DavidMcMorris <david.mcmorris92@gmail.com>
2025-12-20 15:32:22 -0500

    Included number of lights in list

--- Day 10/Day10.R
+++ Day 10/Day10.R
@@ -4,8 +4,10 @@ input <- readLines(input_file) |> strsplit(split = " ")
 machines <- list()
 
 for (i in seq_along(input)) {
+  n <- nchar(input[[i]][1]) - 2
   l <- which((input[[i]][1] |> strsplit(split = ""))[[1]] == "#")
   b <- input[[i]][-c(1, length(input[[i]]))]
   j <- input[[i]][length(input[[i]])]
-  machines[[i]] <- list("lights" = l - 2, "buttons" = b, "joltage" = j)
+  machines[[i]] <- list("n" = n, "lights" = l - 2, "buttons" = b, "joltage" = j)
 }
+
