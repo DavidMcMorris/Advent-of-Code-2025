de4992e498f88cfa188b882faf6a877c24c285e3
DavidMcMorris <david.mcmorris92@gmail.com>
2025-12-20 15:56:00 -0500

    using binary to label each configuration

--- Day 10/Day10.R
+++ Day 10/Day10.R
@@ -1,3 +1,5 @@
+require(R.utils, stringr)
+
 input_file <- "sample.txt"
 input <- readLines(input_file) |> strsplit(split = " ")
 
@@ -11,3 +13,5 @@ for (i in seq_along(input)) {
   machines[[i]] <- list("n" = n, "lights" = l - 2, "buttons" = b, "joltage" = j)
 }
 
+n <- machines[[1]]$n
+verts <- intToBin(0:((2^n) - 1)) |> str_pad(n, side = "left", pad = "0")
